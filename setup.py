fa315f34fdb4b1638508350e4caa5edf839de516
idnm <nnemkov@gmail.com>
2022-04-12 22:53:20 +0300

    Remove extra from setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
      author="Nikita Nemkov",
      author_email="nnemkov@gmail.com",
      description="Variational synthesis of quantum circuits.",
-     url="",
+     url="https://github.com/idnm/testcpflow",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
@@ -18,20 +18,3 @@ setuptools.setup(
          "Operating System :: OS Independent",
      ],
  )
-
-from setuptools import setup
-
-setup(name='testcpflow',
-      packages=['cpflow'],
-      version='1.0.1',
-      description='Brief Description HERE',
-      url='https://github.com/idnm/testcpflow',
-      download_url='',  # FILL IN LATER
-      author='Nikita Nemkov',
-      author_email='nnemkov@gmail.com',
-      keywords=['quantum', 'circuits', 'decomposition', 'synthesis'],
-      license='MIT',
-
-      install_requires=['pandas', 'numpy', 'jax', 'jaxlib'],
-
-      )
