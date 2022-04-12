32c5e33cfa83b129751c5098d09195a317abf46d
idnm <nnemkov@gmail.com>
2022-04-12 21:29:23 +0300

    Add setup.py and setup.cfg .

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(name='testcpflow',
+      packages=['cpflow'],
+      version='1.0.0',
+      description='Brief Description HERE',
+      url='https://github.com/idnm/testcpflow',
+      download_url='',  # FILL IN LATER
+      author='Nikita Nemkov',
+      author_email='nnemkov@gmail.com',
+      keywords=['quantum', 'circuits', 'decomposition', 'synthesis'],
+      license='MIT',
+
+      install_requires=['pandas', 'numpy', 'jax'],
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Quantum engineers',
+          'Topic :: Quantum Computing :: Circuit Synthesis',
+          'License :: OSI Approved :: MIT License',  # Your License Here
+          'Programming Language :: Python :: ^3.7',
+      ],
+      )
