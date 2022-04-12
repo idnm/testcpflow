9d6010f282c56ae9b118adfd8f276293f3dc6ffa
idnm <nnemkov@gmail.com>
2022-04-12 22:17:43 +0300

    go to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='testcpflow',
       packages=['cpflow'],
-      version='1.0.0',
+      version='1.0.1',
       description='Brief Description HERE',
       url='https://github.com/idnm/testcpflow',
-      download_url='https://github.com/idnm/testcpflow/archive/refs/tags/1.0.0.tar.gz',  # FILL IN LATER
+      download_url='',  # FILL IN LATER
       author='Nikita Nemkov',
       author_email='nnemkov@gmail.com',
       keywords=['quantum', 'circuits', 'decomposition', 'synthesis'],
